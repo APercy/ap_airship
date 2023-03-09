c6b15acc0a5e6851362627cd523c87be5915c4f9
Alexsandro Percy <alexsandro.percy@gmail.com>
2023-03-08 21:56:34 -0300

    fix player pos after leaving chair

--- entities.lua
+++ entities.lua
@@ -218,6 +218,7 @@ local function right_click_chair(self, clicker)
                 end
             else
                 ship_self._passenger_is_sit[index] = 0
+                player_api.set_animation(clicker, "stand")
             end
         end
     end
