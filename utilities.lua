2573c9a1c3d208ed3c8067b54f4c8dfc4cd7924c
Alexsandro Percy <alexsandro.percy@gmail.com>
2023-05-20 13:10:30 -0300

    fixed position movements above 5

--- utilities.lua
+++ utilities.lua
@@ -156,7 +156,7 @@ function ap_airship.attach_pax(self, player, slot)
 
     --now yes, lets attach the player
     --randomize the seat
-    local t = {1,2,3,4,5}
+    local t = {1,2,3,4,5,6,7,8,9,10,11,12}
     for i = 1, #t*2 do
         local a = math.random(#t)
         local b = math.random(#t)
